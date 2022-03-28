f9ddc381d2de81b4649a8d13843414c29f6c3bd9
retraigo <nett@nekooftheabyss.xyz>
2022-03-28 16:52:00 +0530

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "virtcert",
+    title: "IVCMASM 2022 Certificate",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
